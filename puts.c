54e3c7ecd2a97800f65a2d85f7c235e9e8bf0be5
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-12-06 10:55:20 +0100

    Initial commit

---
+++ puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle"); /* Print a message */
+    return (0); /* Return success */
+}
+
---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Hello, World!"); /* Print a message */
+return (0); /* Return success */
+}
+

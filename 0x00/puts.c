024bd66a7f28cf62c911e524ebc5379acb40a09d
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-12-06 10:57:48 +0100

    Fixed betty error

--- 0x00/puts.c
+++ 0x00/puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle"); /* Print a message */
-    return (0); /* Return success */
+puts("\"Programming is like building a multilingual puzzle");
+return (0); /* Return success */
 }
 

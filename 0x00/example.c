80338f6c667339dcb0f788c90bb8ccb5e0b1fe9b
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-12-06 16:26:06 +0100

    Learnt how to use printf

---
+++ 0x00/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Hello, %s! You are %d years old.\n", "Al-Areef", 18);
+return (0);
+}
